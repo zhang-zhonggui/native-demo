0fd2311d69c382478ebb5c588e9562e71160ecc1
张中贵 <2521250151@qq.com>
2024-06-01 22:32:31 +0800

    Initial commit

---
+++ src/main/java/com/zzg/nativedemo/web/DemoController.java
@@ -0,0 +1,19 @@
+package com.zzg.nativedemo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhang
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+
+    @GetMapping
+    public String getDemo() {
+        return "hello word";
+    }
+}
